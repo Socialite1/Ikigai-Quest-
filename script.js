03d09a656e07f8e7ea0dc62753b8000d69c5bace
Socialite1 <tgsekgololo@gmail.com>
2026-03-10 14:05:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function saveChapter1(){
 
+let love = document.getElementById("love").value;
+let skill = document.getElementById("skill").value;
+let paid = document.getElementById("paid").value;
+let need = document.getElementById("need").value;
+
+localStorage.setItem("love",love);
+localStorage.setItem("skill",skill);
+localStorage.setItem("paid",paid);
+localStorage.setItem("need",need);
+
+window.location.href="chapter2.html";
+
+}
