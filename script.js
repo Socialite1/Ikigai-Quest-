6c65d260bf864d01519ddae85d249dd6465247c4
Socialite1 <tgsekgololo@gmail.com>
2026-03-10 14:07:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ localStorage.setItem("need",need);
 window.location.href="chapter2.html";
 
 }
+
+function saveChapter2(){
+
+localStorage.setItem("goal",document.getElementById("goal").value);
+localStorage.setItem("benefits",document.getElementById("benefits").value);
+localStorage.setItem("obstacles",document.getElementById("obstacles").value);
+localStorage.setItem("solutions",document.getElementById("solutions").value);
+localStorage.setItem("deadline",document.getElementById("deadline").value);
+
+window.location.href="chapter3.html";
+
+}
+
